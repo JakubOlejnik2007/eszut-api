3944cef7a024f6957c4c003e80812fc8b0afb8d3
JakubOlejnik2007 <jacobole2000@gmail.com>
2024-09-30 22:15:06 +0200

    Changed welcome line after copying .env file

--- copy-env.js
+++ copy-env.js
@@ -3,4 +3,4 @@ const path = require("path");
 const envFilePath = path.resolve(__dirname, ".env");
 const distFolderPath = path.resolve(__dirname, "dist");
 fs.copyFileSync(envFilePath, path.join(distFolderPath, ".env"));
-console.log('.env file has been copied to the "dist" folder. We suggest to create root user by using create-root-user from utils.');
+console.log('.env file has been copied to the "dist" folder.');
