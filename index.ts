f560a28e972914ea82e23c20e177b39abfe67b6b
Jakub Olejnik <87308273+JakubOlejnik2007@users.noreply.github.com>
2025-01-31 00:24:51 +0100

    Fixed cors policy

--- index.ts
+++ index.ts
@@ -43,7 +43,24 @@ const app: Express = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+    cors({
+        origin: ["http://localhost:5173", "https://eszut-api.tenco.waw.pl", "https://eszut.tenco.waw.pl"],
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true,
+        exposedHeaders: ["Authorization"], // Dodanie tej opcji może pomóc
+    })
+);
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
+
 
 /*
     PUBLIC ROUTES
